21701e7f46cc9da0b2066e79d70f41306a1d3f9f
miao3hu2 <miao3hu2@gmail.com>
2026-09-17 14:01:10 +0200

    adding evaluation sample module

---
+++ src/evaluation/observables.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from core.data.field import Field
+
+
+def magnetization(field: Field) -> np.ndarray:
+
+    spins = field.values
+
+    if field.batched:
+        axes = tuple(range(1, spins.ndim))
+        return spins.mean(axis=axes)
+    else:
+        return spins.mean()
+
+
+
+def mean_magnetization(field: Field) -> float:
+    if field.batched:
+        return float(
+            np.mean(magnetization(field))
+        )
+    else:
+        return float(magnetization(field))
+
+
+def susceptibility(field: Field, beta: float) -> float:
+
+    m = magnetization(field)
+
+    shape = field.values.shape[1:] if field.batched else field.values.shape
+    N = np.prod(shape)
+
+    return beta * N * np.var(m)
